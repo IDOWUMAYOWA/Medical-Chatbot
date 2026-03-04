8225d296756527ee5f33753c11366f8da856121c
IDOWUMAYOWA <sangotadeidowu@yahoo.com>
2026-03-04 18:00:35 +0100

    Project Structure has been created

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating Main directories
+mkdir -p src
+mkdir -p research 
+
+# Creating files
+touch src/__init__.py
+touch src/utils.py 
+touch src/prompt.py
+touch .env 
+touch setup.py
+touch web_app.py
+touch research/trials.ipynb 
+touch requirements.txt
+
+
+echo "Directory and files has been created successfully!."
